77f74a9d57aa459b7b35468e0de755fabd24029d
vicmescan <mestrecanto@gmail.com>
2023-01-18 12:59:10 +0100

    improved links in header and footer, background of menu page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,22 +6,36 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 
 function Footer() {
   return (
-    <CardGroup className="footer">
+    <CardGroup fixed="bottom" className="footer">
       <Card className="text-center footer-card">
         <Card.Body>
           <Card.Title>CONTACTO</Card.Title>
           <hr></hr>
           <Card.Text>
-            <a
-              href="https://goo.gl/maps/3xMkcCjUHRJme8ao6"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <p>Dresdenner Straße 120</p>
-            </a>
+            <p className="footer-link">
+              {" "}
+              <a
+                href="https://goo.gl/maps/3xMkcCjUHRJme8ao6"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Dresdenner Straße 120{" "}
+              </a>
+            </p>
+
             <p>10999 Berlin</p>
             <p>Tel: 030-55624038</p>
-            <p>info@cevicheria-berlin.com</p>
+
+            <p className="footer-link">
+              {" "}
+              <a
+                href={"mailto: info@cevicheria-berlin.com"}
+                target="_blank"
+                rel="noreferrer"
+              >
+                info@cevicheria-berlin.com{" "}
+              </a>
+            </p>
           </Card.Text>
         </Card.Body>
       </Card>
@@ -30,37 +44,43 @@ function Footer() {
           <Card.Title>HORARIO</Card.Title>
           <hr></hr>
           <Card.Text>
-            <p> De Lunes a Sabado desde las 18h</p>
-            <p> Domingos Cerrados</p>
+            <p> De Lunes a Sábado desde las 18h</p>
+            <p> Domingos Cerrado</p>
           </Card.Text>
           <div className="footer-icons">
-            <a
-              href="https://goo.gl/maps/3xMkcCjUHRJme8ao6"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IconContext.Provider value={{ size: "2.6rem" }}>
-                <FaMapMarkerAlt />
-              </IconContext.Provider>
-            </a>
-            <a
-              href="https://www.facebook.com/CevicheriaBerlin/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IconContext.Provider value={{ size: "2.5rem" }}>
-                <BsFacebook />
-              </IconContext.Provider>
-            </a>
-            <a
-              href="https://www.instagram.com/cevicheria.berlin/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <IconContext.Provider value={{ size: "3rem" }}>
-                <AiFillInstagram />
-              </IconContext.Provider>
-            </a>
+            <div className="footer-link">
+              <a
+                href="https://goo.gl/maps/3xMkcCjUHRJme8ao6"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <IconContext.Provider value={{ size: "2.6rem" }}>
+                  <FaMapMarkerAlt />
+                </IconContext.Provider>
+              </a>
+            </div>
+            <div className="footer-link">
+              <a
+                href="https://www.facebook.com/CevicheriaBerlin/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <IconContext.Provider value={{ size: "2.5rem" }}>
+                  <BsFacebook />
+                </IconContext.Provider>
+              </a>
+            </div>
+            <div className="footer-link">
+              <a
+                href="https://www.instagram.com/cevicheria.berlin/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <IconContext.Provider value={{ size: "3rem" }}>
+                  <AiFillInstagram />
+                </IconContext.Provider>
+              </a>
+            </div>
           </div>
         </Card.Body>
       </Card>
--- src/routes/Menu.js
+++ src/routes/Menu.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/menu.css";
 
 export default function Menu() {
   return (
     <>
       <Header />
-      <h1>Menu</h1>
+      <div className="menu-container">
+        <h1>Menu</h1>
+      </div>
+      <Footer />
     </>
   );
 }
