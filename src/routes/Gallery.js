5bb07009722d39d05c3457d82a416110f17d5333
vicmescan <mestrecanto@gmail.com>
2023-01-27 13:00:24 +0100

    cleaner code

--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import ScrollUp from "..";
 import GalleryPic from "../components/GalleryPic";
 import one from "../images/galleryImages/1.jpeg";
