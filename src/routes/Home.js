2761f596d2d06bed94a3b8f9ebef645ceb8470ab
vicmescan <mestrecanto@gmail.com>
2023-03-03 12:31:42 +0100

    added coockies banner

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,17 +3,28 @@ import HomeCarousel from "../components/home/HomeCarousel";
 import Footer from "../components/common/Footer";
 import HomeQuote from "../components/home/HomeQuote";
 import ScrollUp from "..";
+import CookieConsent from "react-cookie-consent";
 
 export default function Root() {
-
   ScrollUp();
 
-    return (
-      <>
-        <Header />
-        <HomeCarousel />
-        <HomeQuote />
-        <Footer />
-      </>
-    );
-  }
+  return (
+    <>
+      <Header />
+      <HomeCarousel />
+      <HomeQuote />
+      <Footer />
+      <CookieConsent
+        location="bottom"
+        buttonText="¡Chevere!"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        Diese Website verwendet Cookies, um die Benutzerfreundlichkeit zu
+        erhöhen.
+      </CookieConsent>
+    </>
+  );
+}
